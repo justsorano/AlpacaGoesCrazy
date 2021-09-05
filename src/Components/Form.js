e8c32a4c44301e594a7acbabb4282f2cd76d9eed
sorano <rokudo.ananas.mukuro@gmail.com>
2021-09-05 22:44:45 +0300

    наброски формы

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,28 +3,34 @@ import React, { useState } from 'react'
 export const Form = () =>{
    const [value,setValue] = useState({
       continent:'',
-      region:''
+      country:''
    })
    const inputHandler = e =>{
-      setValue({
+      setValue(prev =>({
+         ...prev,
          [e.target.name]:e.target.value
-      })
+      }))
+   }
+   const submitHandler = e =>{
+      e.preventDefault()
+      const newArray = {...value}
+      console.log(newArray)
    }
    return (
-      <div className='form'>
+      <div className='form' onSubmit={submitHandler}>
          <form className='form__contant'>
             <input className='form__input'
-            value={value}
             name='continent'
             value={value.continent}
             placeholder='Tap continent...'
-            onChange={inputHandler}/>
+            onChange={inputHandler}
+            required/>
             <input className='form__input'
-            value={value}
-            name='region'
-            value={value.region}
-            placeholder='Tap region...'
-            onChange={inputHandler}/>
+            name='country'
+            value={value.country}
+            placeholder='Tap country...'
+            onChange={inputHandler}
+            required/>
             <button className='form__submit'>push</button>
          </form>
       </div>
